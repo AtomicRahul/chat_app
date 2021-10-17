803ef6f4279dc08ffb1f4c4abe4ca65b90f58548
AtomicRahul <rahul0072832001@gmail.com>
2021-10-17 23:03:10 +0530

    more changes

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/services/methods.dart';
 import 'package:chat_app/views/signin.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/widgets/widgets.dart';
@@ -10,136 +11,173 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-  TextEditingController userNameTextEditingController =
-      new TextEditingController();
-  TextEditingController emailTextEditingController =
-      new TextEditingController();
-  TextEditingController passwordTextEditingController =
-      new TextEditingController();
+  TextEditingController _name = new TextEditingController();
+  TextEditingController _email = new TextEditingController();
+  TextEditingController _password = new TextEditingController();
+
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SingleChildScrollView(
+    final size = MediaQuery.of(context).size;
+
+    return isLoading
+        ? Center(
             child: Container(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 50),
-                child: Column(
-                  children: [
-                    TextField(
-                        controller: userNameTextEditingController,
-                        style: simpleTextFieldStyle(),
-                        decoration: textFieldInputDecoration("Username")),
-                    SizedBox(height: 30),
-                    TextField(
-                        controller: emailTextEditingController,
-                        style: simpleTextFieldStyle(),
-                        decoration: textFieldInputDecoration("Email")),
-                    SizedBox(height: 30),
-                    TextField(
-                        controller: passwordTextEditingController,
-                        style: simpleTextFieldStyle(),
-                        decoration: textFieldInputDecoration("Password")),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Container(
-                      alignment: Alignment.centerRight,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 16),
-                        child: Text(
-                          "Forget Password?",
-                          style: simpleTextFieldStyle(),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.blue),
-                      child: Text(
-                        "Create Account",
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: Padding(
-                          padding: const EdgeInsets.only(right: 20, left: 20),
-                          child: Divider(
-                            color: Colors.black,
-                          ),
-                        )),
-                        Text(
-                          "OR",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        Expanded(
-                            child: Padding(
-                          padding: const EdgeInsets.only(right: 20, left: 20),
-                          child: Divider(
-                            color: Colors.black,
-                          ),
-                        ))
-                      ],
-                    ),
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.blue),
-                      child: Text(
-                        "Sign Up With Google",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    SizedBox(height: 50),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Already Have an Account?  ",
-                          style: simpleTextFieldStyle(),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SignIn()));
-                          },
-                          child: Text(
-                            "Sign in",
-                            style: mediumTextFieldStyle(),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              height: size.height / 20,
+              width: size.height / 20,
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ],
-      ),
-    );
+          )
+        : Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SingleChildScrollView(
+                  child: Container(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 50),
+                      child: Column(
+                        children: [
+                          TextField(
+                              controller: _name,
+                              style: simpleTextFieldStyle(),
+                              decoration: textFieldInputDecoration("Username")),
+                          SizedBox(height: 30),
+                          TextField(
+                              controller: _email,
+                              style: simpleTextFieldStyle(),
+                              decoration: textFieldInputDecoration("Email")),
+                          SizedBox(height: 30),
+                          TextField(
+                              controller: _password,
+                              style: simpleTextFieldStyle(),
+                              decoration: textFieldInputDecoration("Password")),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Container(
+                            alignment: Alignment.centerRight,
+                            child: Container(
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                "Forget Password?",
+                                style: simpleTextFieldStyle(),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          GestureDetector(onTap: () {
+                            if (_name.text.isNotEmpty &&
+                                _email.text.isNotEmpty &&
+                                _password.text.isNotEmpty) {
+                              setState(() {
+                                isLoading = true;
+                              });
+
+                              createAccount(
+                                      _name.text, _email.text, _password.text)
+                                  .then((user) {
+                                if (user != null) {
+                                  setState(() {
+                                    isLoading = false;
+                                  });
+                                  print("Login Successfull");
+                                } else {
+                                  print("Login Failed");
+                                }
+                              });
+                            } else {
+                              print("Plese Enter Fields");
+                            }
+
+                            Container(
+                              alignment: Alignment.center,
+                              width: MediaQuery.of(context).size.width,
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Colors.blue),
+                              child: Text(
+                                "Create Account",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.white),
+                              ),
+                            );
+                          }),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                  child: Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 20, left: 20),
+                                child: Divider(
+                                  color: Colors.black,
+                                ),
+                              )),
+                              Text(
+                                "OR",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              Expanded(
+                                  child: Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 20, left: 20),
+                                child: Divider(
+                                  color: Colors.black,
+                                ),
+                              ))
+                            ],
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            width: MediaQuery.of(context).size.width,
+                            padding: EdgeInsets.symmetric(vertical: 15),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.blue),
+                            child: Text(
+                              "Sign Up With Google",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          SizedBox(height: 50),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Already Have an Account?  ",
+                                style: simpleTextFieldStyle(),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => SignIn()));
+                                },
+                                child: Text(
+                                  "Sign in",
+                                  style: mediumTextFieldStyle(),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
